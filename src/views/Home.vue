55a9f85212aafbe2d5fd5dd14ccab131e429d8a5
ronb-tech <ronbersh@gmail.com>
2024-04-10 19:59:59 +0300

    add cat tabs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="home-page">
     <h1>Home page</h1>
-    <div v-if="!loading && !error && wordCloudData">
-      <WordCloud :data="wordCloudData"></WordCloud>
+    <div class="word-cloud-section" v-if="!loading && !error && filteredData">
+      <div class="tabs">
+        <span
+          v-for="category in categories"
+          :key="category"
+          @click="onSelectedCategory(category)"
+          :class="{ active: category === selectedCategory }"
+        >
+          {{ category }}
+        </span>
+      </div>
+      <WordCloud :data="filteredData"></WordCloud>
     </div>
 
     <div v-if="loading">
@@ -15,7 +25,7 @@
 </template>
 
 <script setup lang="js">
-import { onMounted, ref } from "vue";
+import { onMounted, ref,computed } from "vue";
 import { useStore } from "vuex";
 import WordCloud from "@/components/WordCloud.vue";
 import { getWordCloudData } from "@/service/index.js";
@@ -24,11 +34,24 @@ const store = useStore();
 const wordCloudData = ref(null);
 const error = ref(null);
 const loading = ref(false);
+const selectedCategory = ref(null);
+const categories = ref([]);
+
+const filteredData = computed(() => {
+  if (!selectedCategory.value) return wordCloudData.value;
+  //return wordCloudData.value.filter(item => Object.key(item) === selectedCategory.value);
+  return wordCloudData.value
+});
 
 onMounted(async () => {
   try {
     loading.value = true;
     wordCloudData.value = await getWordCloudData();
+    if(wordCloudData.value){
+      categories.value= Object.keys(wordCloudData.value);
+      selectedCategory.value= categories.value[0];
+
+    }
     loading.value = false;
   } catch (e) {
     console.log("Failed to load word cloud data:", e);
@@ -36,6 +59,11 @@ onMounted(async () => {
     loading.value = false;
   }
 });
+
+const onSelectedCategory=(category)=>{
+  console.log("Selected category", category);
+  selectedCategory.value = category
+}
 </script>
 
 <style lang="scss" scoped>
@@ -46,5 +74,25 @@ onMounted(async () => {
   gap: $spacing-xl;
   align-items: center;
   justify-content: center;
+
+  .word-cloud-section {
+    display: flex;
+    flex-direction: column;
+    gap: $spacing-xl;
+    align-items: center;
+    justify-content: center;
+
+    .tabs {
+      display: flex;
+      gap: $spacing-lg;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      font-weight: bold;
+      text-decoration: underline;
+      text-transform: capitalize;
+      font-size: $font-size-sm;
+    }
+  }
 }
 </style>
